956830eb5619b8ff1806cb74e164894be4c10eec
tu anh <tubato1999@gmail.com>
2022-01-17 02:29:12 +0700

    hoan thanh search thong tin khac

--- app/controllers/searchImformationController.js
+++ app/controllers/searchImformationController.js
@@ -70,12 +70,25 @@ exports.searchbyName = async(req, res) => {
                     });
                     noidung2 = "";
                 }
-                
-                res.status(200).json({
-                    "data" : content,
-                    "percent": out,
-                    "message": "Successfull"
-                });
+
+                output =  await Thong_tin_y_te_khac.find({"tieu_de": content});
+
+                if(out>0.7)
+                {
+                    res.status(200).json({
+                        "data" : output,
+                        "percent": out,
+                        "message": "Successfull"
+                    });
+                }
+                else
+                {
+                    res.status(200).json({
+                        "data" : [],
+                        "value": name,
+                        "message": "Successfull"
+                    });
+                }
             }
             else
             {
@@ -94,6 +107,87 @@ exports.searchbyName = async(req, res) => {
     } catch (error) {
         res.status(500).json({ "message": "Server Error" });
         console.log(error);
+    }
+
+};
+
+exports.searchbyNameDialog = async(req, res) => {
+    
+    try {
+        var name = req.query.name;
+        url_api = encodeURI(link + `/stopword?content=${name}`) ;
+        await axios.get(url_api).then(function(response){name = response.data["content"];});
+        var data;
+        if (name) {
+            data = await Thong_tin_y_te_khac.find({$text: {$search: name}});
+            if(data.length >0)
+            {
+                var noidung2 = "";
+                var count = 1;
+                var content;
+                var out = 0;
+                for(var i = 0; i<data.length; i++)
+                {
+                    if(count == 1)
+                    {
+                        noidung2 = data[i]["tieu_de"]
+                    }
+                    else
+                    {
+                        noidung2 += " , " + data[i]["tieu_de"]
+                    }
+                    if(count%70 == 0)
+                    {
+                        url_api = encodeURI(link + `/sosanh?noidung1=${name}&noidung2=${noidung2}`) ;
+                        await axios.get(url_api).then(function(response)
+                        {
+                            if(out<response.data["phan_tram"])
+                            {
+                                content =  response.data["content"]
+                                out = response.data["phan_tram"]
+                            }
+                        });
+                        noidung2 = "";
+                    }
+                    count ++;
+                }
+                if(count%70 > 0)
+                {
+                    url_api = encodeURI(link + `/sosanh?noidung1=${name}&noidung2=${noidung2}`) ;
+                    await axios.get(url_api).then(function(response)
+                    {
+                        if(out<response.data["phan_tram"])
+                        {
+                            content =  response.data["content"]
+                            out = response.data["phan_tram"]
+                        }
+                        
+                    });
+                    noidung2 = "";
+                }
+                output =  await Thong_tin_y_te_khac.find({"tieu_de": content});
+
+                if(out>0.7)
+                {
+                    return output;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+        {
+            return null;
+        }
+
+    } catch (error) {
+        return null;
     }
 
 };
